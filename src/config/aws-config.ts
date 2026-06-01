8c22ab365011c55839538f29562d7ce26f079499
liu1138 <liu1138@ebank2001.domain>
2026-06-01 16:54:02 +0900

    github action

--- src/config/aws-config.ts
+++ src/config/aws-config.ts
@@ -15,12 +15,12 @@
  * Set to false when connecting to a real Cognito User Pool.
  */
 export const DEV_MOCK_AUTH = false;
-
+import { USER_POOL_ID, USER_POOL_CLIENT_ID } from '@env';
 export const awsConfig = {
   Auth: {
     Cognito: {
-      userPoolId: process.env.USER_POOL_ID,
-      userPoolClientId: process.env.USER_POOL_CLIENT_ID,
+      userPoolId: `${USER_POOL_ID}`,
+      userPoolClientId: `${USER_POOL_CLIENT_ID}`,
       loginWith: {
         email: true,
         phone: true,
